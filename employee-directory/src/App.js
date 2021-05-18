abdfa90a5e1e7198ff0e148d6fb7e428b8563e4a
Thomas Elder <MasonCosLive@Mason-Cos-Macbook.local>
2021-05-18 11:30:38 -0600

    Made route not specific

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -12,7 +12,7 @@ function App() {
       <div>
         <Navbar />
         <Wrapper>
-          <Route exact path="/" component={Home} />
+          <Route path="/" component={Home} />
         </Wrapper>
         <Footer />
       </div>
